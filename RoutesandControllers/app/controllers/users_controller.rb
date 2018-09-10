11998294a667a4c0040a5277d532ac5271b4f658
elopez17 <e_lo28@yahoo.com>
2018-09-10 15:14:59 -0700

    ad user controller methods

--- RoutesandControllers/app/controllers/users_controller.rb
+++ RoutesandControllers/app/controllers/users_controller.rb
@@ -1,13 +1,45 @@
 class UsersController < ApplicationController 
   def index
-    render json: params
+    users = User.all
+    render json: users
   end
   
   def create
-    render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: 422
+    end 
   end
   
+  def update 
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: 422
+    end 
+    
+  end 
+  
   def show
-    render json: params
+    user = User.find(params[:id])
+    if user
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: 422
+    end 
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    render plain: "User deleted"
+  end
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end 
 end 
--- RoutesandControllers/app/models/user.rb
+++ RoutesandControllers/app/models/user.rb
@@ -1,3 +1,4 @@
-class User < ApplicationController
+class User < ApplicationRecord 
+  validates :username, :email, presence: true, uniqueness: true
 end 
 
